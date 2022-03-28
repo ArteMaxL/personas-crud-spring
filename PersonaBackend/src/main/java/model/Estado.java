1fa7714e680e02c4e2f2f122f62af2d0d30a2551
ArteMaxL <artemiolucero@gmail.com>
2022-03-27 23:26:07 -0300

    Modelo Persona, Pais, Estado

--- PersonaBackend/src/main/java/model/Estado.java
+++ PersonaBackend/src/main/java/model/Estado.java
@@ -1,10 +1,44 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="estado")
 public class Estado {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	
 	private String nombre;
 	
+	@ManyToOne
+	@JoinColumn(name="id_pais")
 	private Pais pais;
-
+	
+	public Estado(String nombre, Pais pais) {
+		super();
+		this.nombre = nombre;
+		this.pais = pais;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	
+	
 }
